d5e770079859cd70f0e227a6e1530d00df686c0a
mythyrian <93193451+mythyrian@users.noreply.github.com>
2022-11-11 16:46:16 +0100

    Add files via upload

--- Mercury/src/main/java/Model/Utilities.java
+++ Mercury/src/main/java/Model/Utilities.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
 
 public class Utilities {
@@ -38,24 +39,32 @@ public class Utilities {
 		}
 	}
 	
-	public static void aggiungiEnte(Ente ente) throws SQLException {
+	public static void aggiungiEnte(Ente ente) throws SQLIntegrityConstraintViolationException {
 		
 		Connection con = connessione();
-	    Statement st=con.createStatement();
+	    
 	    
 /* inserimento dati dell'ente nella tabella utente */	    
 		try {
+			Statement st=con.createStatement();
 	    String queryIns = " INSERT INTO utente ( nome, cognome, email, password, ruolo) "
 	    		         + " VALUES ('"+ ente.getNomeResponsabile() +"','"+ ente.getCognomeResponsabile() + "','" + ente.getEmail() 
 	    		         + "','" + ente.getPassword() + " ','ente');";
-	    
-	    st.executeUpdate(queryIns);} 
+	    st.executeUpdate(queryIns);
+	    } 
+		
+		catch(SQLIntegrityConstraintViolationException e){
+	    	   
+	    	   throw e ;
+	    	   }
 		
 		catch(SQLException e){
 	    	   
 	    	   System.out.println(" errore aggiungi utente");
 	    	   e.printStackTrace();
-	    	   };
+	    	   }
+	    	   
+	    	   
 	    	   
 /* inserimento dati dell'ente nella tabella ente  */	   	    	   
 		try {
@@ -64,6 +73,7 @@ public class Utilities {
 	        	        
 /* try per estrarre chiave id dell'utente */
 	         try {
+	        	 Statement st=con.createStatement();
 	 	        ResultSet rst = st.executeQuery("SELECT idUtente from utente where email ='" + ente.getEmail() + "'");
 	 	        
 	 	       while(rst.next()) { idUtente = rst.getInt("idUtente");}
@@ -77,7 +87,7 @@ public class Utilities {
 /* con id utente si va ad fare un INSERT nella tabella Ente   */                 
 	    String queryIns = " INSERT INTO ente ( nomeEnte, stato, utenteFK) "
 	    		         + " VALUES ('"+ ente.getNomeEnte() + " ','attivo','" + idUtente + "');";
-	    
+	    Statement st=con.createStatement();
 	    st.executeUpdate(queryIns);} 
 		
 		catch(SQLException e){   
@@ -85,4 +95,48 @@ public class Utilities {
 	    	   e.printStackTrace();
 	    	   }  
 	}
+
+    public static Object logIn(String email, String password) throws SQLException {
+    
+    	Connection con = connessione();
+	    Statement st=con.createStatement();
+	    Object utente = null;
+	    
+	    try {
+	    	String query = "Select * from utente where email='" + email + "' AND password='" + password + "'";
+	    	ResultSet rst = st.executeQuery(query);
+	    	while(rst.next()) {
+	    		if(rst.getString("ruolo").equals("ente")) {
+	    			Ente ente = new Ente(rst.getString("email"), rst.getString("password"),
+	    					    rst.getString("idUtente"), rst.getString("nome"), rst.getString("cognome"));
+	    			
+	    			query = "Select nomeEnte from ente where utenteFK='" + ente.getNomeEnte()+"'";
+	    			rst = st.executeQuery(query);
+	    			
+	    			while(rst.next()) {
+	    				ente.setNomeEnte(rst.getString("nomeEnte"));
+	    			}
+	    			utente = ente;
+	    			
+	    		}
+	    		else {            
+	    			Amministratore admin = new Amministratore(rst.getString("email"), rst.getString("password"),
+    					    rst.getString("nome"), rst.getString("cognome"));
+	    			utente = admin;
+	    			
+	    		}
+	    	}
+	    	
+			
+		} 	catch(SQLException e){
+	    	   
+	    	   System.out.println(" errore log in");
+	    	   e.printStackTrace();
+	    	   };
+	    	   	    
+	    return utente;
+    	
+    	
+    }
+
 }
