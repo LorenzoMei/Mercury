9f93a7e24488006f7e77cc7a8f33ccb113268942
IngLuciano92 <94065037+IngLuciano92@users.noreply.github.com>
2022-11-15 15:28:06 +0100

    Add files via upload

---
+++ Mercury/src/main/java/controller/ServletFiltro.java
@@ -0,0 +1,69 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Utilities;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class ServletFiltro
+ */
+public class ServletFiltro extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try (PrintWriter out = response.getWriter()) {
+            
+            String op = request.getParameter("operation");
+            
+            if (op.equals("regione")) {
+                List<String> regioni = Utilities.getRegione();
+                Gson json = new Gson();
+                String regione = json.toJson(regioni);
+                System.out.print(regione);
+                response.setContentType("text/html");
+                response.getWriter().write(regione);
+            }
+
+            if (op.equals("provincia")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+                List<String> slist = Utilities.getProvincia(id);
+                Gson json = new Gson();
+                String countryList = json.toJson(slist);
+                response.setContentType("text/html");
+                response.getWriter().write(countryList);
+            }
+
+            if (op.equals("city")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+                List<String> comuni = Utilities.getComune(id);
+                Gson json = new Gson();
+                String comune = json.toJson(comuni);
+                response.setContentType("text/html");
+                response.getWriter().write(comune);
+            }
+        }
+    }
+		
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
