ac2472feac7f3db848b5dd2606f9a91cbbf695bc
Lorenzo Mei <45484506+LorenzoMei@users.noreply.github.com>
2022-11-15 15:06:23 +0100

    Add files via upload

---
+++ Mercury/src/main/java/controller/ServletFiltra.java
@@ -0,0 +1,99 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Evento;
+import model.Utilities;
+import model.Zona;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Servlet implementation class ServletFiltra
+ */
+public class ServletFiltra extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletFiltra() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		List<Evento> listaEventi = (List<Evento>) request.getAttribute("listaEventi");
+		List<Evento> listaEventiFiltrata = new ArrayList<Evento>();
+		
+		//filtro per tipo
+		String tipo = request.getParameter("tipo");
+		
+		if(tipo != null) {
+			for(int i = 0; i < listaEventi.size(); i++) {
+				if(listaEventi.get(i).getTipo().equals(tipo)) {
+					listaEventiFiltrata.add(listaEventi.get(i));
+					listaEventi.set(i, null);
+				}
+			}
+		}
+		
+		//filtra per data
+		
+		LocalDate dataInizio = null; 
+		LocalDate dataFine = null;
+		
+		if(request.getParameter("dataInizio") != null && request.getParameter("dataFine") != null) {
+			dataInizio = LocalDate.parse(request.getParameter("dataInizio"));
+			 dataFine = LocalDate.parse(request.getParameter("dataFine"));
+		}
+		
+		if(dataInizio != null && dataFine != null) {
+			for(int i = 0; i < listaEventi.size(); i++) {
+				if((listaEventi.get(i).getDataInizio().isAfter(dataInizio) || listaEventi.get(i).getDataInizio().equals(dataInizio)) && 
+						(listaEventi.get(i).getDataFine().isBefore(dataFine) || listaEventi.get(i).getDataFine().equals(dataFine))) {
+					listaEventiFiltrata.add(listaEventi.get(i));
+					listaEventi.set(i, null);
+				}
+			}
+		}
+		
+		//filtra per zona
+		String regione = request.getParameter("regione");
+		String provincia = request.getParameter("provincia");
+		String comune = request.getParameter("comune");
+		
+		if(listaEventiFiltrata.size() != 0) {
+			listaEventiFiltrata = Utilities.filtraEventiPerZona(regione, provincia, comune, listaEventiFiltrata);
+		}
+		else{
+			listaEventiFiltrata = Utilities.filtraEventiPerZona(regione, provincia, comune, listaEventi);
+		}
+		
+		request.setAttribute("listaEventi", listaEventiFiltrata);
+		
+		
+	}
+
+}
